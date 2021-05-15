1192fd9a00ebc9c0e7f1e4a89999a0adb53f40c3
kristianmischke <kristianmischke@gmail.com>
2021-05-15 15:05:00 -0400

    Working on yml -> latex -> pdf resume generation

---
+++ resume/resume_generator.py
@@ -0,0 +1,48 @@
+from jinja2 import Environment
+import os
+import subprocess
+import yaml
+
+
+def main():
+    # get resume data
+    with open('../_data/resume-cv.yml', encoding='utf8') as file:
+        resume_data = yaml.load(file, Loader=yaml.FullLoader)
+
+    # get original templated latex file
+    with open('./resume_template.tex') as f:
+        template_doc = f.read()
+
+    # run template and output progress to file
+    output = Environment(
+        block_start_string='(((',
+        block_end_string=')))',
+        variable_start_string='((',
+        variable_end_string='))',
+        comment_start_string='((#',
+        comment_end_string='#))',
+        trim_blocks=True
+    ).from_string(template_doc).render(resume_data)
+    with open('./resume_output.tex', 'w') as f:
+        f.write(output)
+
+    # build pdf
+    cmd = ['pdflatex', '-interaction', 'nonstopmode', './resume_output.tex']
+    proc = subprocess.Popen(cmd)
+    proc.communicate()
+
+    retcode = proc.returncode
+    if not retcode == 0:
+        os.unlink('resume_output.pdf')
+        raise ValueError('Error {} executing command: {}'.format(retcode, ' '.join(cmd)))
+    os.unlink('resume_output.aux')
+    os.unlink('resume_output.log')
+
+    # open to view file
+    result_pdf_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resume_output.pdf')
+    print(result_pdf_path)
+    os.system(f'cmd /c {result_pdf_path}')
+
+
+if __name__ == '__main__':
+    main()
